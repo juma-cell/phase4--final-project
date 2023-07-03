2bd985cdc132488b75e35e6fbfa37d656b5305f5
Juma <juma.musa@student.moringaschool.com>
2023-07-03 15:48:59 +0300

    done

---
+++ forum/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes ,Route } from "react-router-dom"
+import Home from "./pages/Home" 
+import Layout from "./Layout/Layout"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+import SignUp from "./pages/SignUp"
+import SingleTopic from "./pages/SingleTopic"
+import UpdatePassword from "./pages/UpdatePassword"
+function App() {
+
+
+  return (
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Layout/>}>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/singletopic" element={<SingleTopic />} />
+        <Route path="/updatepassword" element={<UpdatePassword />} />
+
+        </Route>
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ forum/src/pages/SingleTopic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingleTopic() {
+  return (
+    <div>SingleTopic</div>
+  )
+}
+
+export default SingleTopic
