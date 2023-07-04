dc10d4b95a6c3c352c55ba4bdccba5e843870b61
Juma <juma.musa@student.moringaschool.com>
2023-07-04 09:10:12 +0300

    changes made

--- forum/src/Layout/Navbar.jsx
+++ forum/src/Layout/Navbar.jsx
@@ -7,13 +7,12 @@ function Navbar() {
    
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" className="flex items-center">
+  <div className="flex items-center">
       <img src={image2} className="h-8 mr-3" alt="Flowbite Logo" />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Forum</span>
-  </a>
+  </div>
   <div className="flex items-center md:order-2">
       <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-        <span className="sr-only">Open user menu</span>
         <img className="w-8 h-8 rounded-full" src={image1} alt=""/>
       </button>
      
--- forum/src/pages/Login.jsx
+++ forum/src/pages/Login.jsx
@@ -83,12 +83,12 @@ function Login() {
           <Link to='/signup' class="underline" href="">Sign up</Link>
         </p>
 
-        <button
-          type="submit"
-          class="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-        >
-          Login 
+        <button 
+        type="submit"
+         class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          Login
         </button>
+
       </div>
     </form>
   </div>
